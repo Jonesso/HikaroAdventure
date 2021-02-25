de58feae48c580858f28810e1e03451e468f934f
RudeZwloki7 <rudezwloki7@gmail.com>
2021-02-25 11:00:50 +0300

    Add gravity, collision and background

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame, sys
 clock = pygame.time.Clock()
 
 from pygame.locals import *
+
 pygame.init()
 
 pygame.display.set_caption("Hikaro Adventure")
@@ -11,16 +12,36 @@ WINDOW_SIZE = (700, 400)
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
-#player_image = pygame.image.load('zaglushka')
+player_image = pygame.image.load('zaglushka')
 moving_right = False
 moving_left = False
 player_location = [30, 30]
+player_y_momentum = 0
+player_rect = pygame.Rect(player_location[0], player_location[1],
+                          player_image.get_width(), player_image.get_height())
+test_rect = pygame.Rect(100,100,100,50)
 while True:
-    #screen.blit(player_image,player_location)
+    screen.fill((146, 244, 255))
+    screen.blit(player_image, player_location)
+
+    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
+        player_y_momentum = -player_y_momentum
+    else:
+        player_y_momentum += 0.2
+    player_location[1] += player_y_momentum
+
     if moving_right == True:
-        player_location[0] +=4
-    if moving_left== True:
-        player_location[0] -=4
+        player_location[0] += 4
+    if moving_left == True:
+        player_location[0] -= 4
+
+    player_rect.x = player_location[0]
+    player_rect.y = player_location[1]
+
+    if player_rect.colliderect(test_rect):
+        pygame.draw.rect(screen, (255, 0, 0), test_rect)
+    else:
+        pygame.draw.rect(screen, (0, 0, 0), test_rect)
 
     for event in pygame.event.get():
         if event.type == QUIT:
