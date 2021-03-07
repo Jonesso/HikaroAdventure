4d2460bba1967eb7eed0c59ccbee2b54e6c5bd3e
Nikita <huskyray@mail.ru>
2021-03-07 19:58:30 +0300

    add: tile map

--- main.py
+++ main.py
@@ -1,48 +1,35 @@
 import pygame, sys
+from pytmx.util_pygame import load_pygame
 
-clock = pygame.time.Clock() # set up the clock
+clock = pygame.time.Clock()  # set up the clock
 
 from pygame.locals import *
-pygame.init() # initiate pygame
 
-pygame.display.set_caption('Pygame Window') # set the window name
+pygame.init()  # initiate pygame
 
-WINDOW_SIZE = (600,400) # set up window size
+pygame.display.set_caption('Pygame Window')  # set the window name
 
-screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate screen
+WINDOW_SIZE = (1280, 720)  # set up window size
 
-display = pygame.Surface((300, 200))
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # initiate screen
 
+display = pygame.Surface((640, 360))
 
-true_scroll = [0,0]  # x, y
-
-def load_map(path):
-    f = open(path,'r')
-    data = f.read()
-    f.close()
-    data = data.split('\n')
-    game_map = []
-    for row in data:
-        game_map.append(list(row))
-    return game_map
-
-game_map = load_map('res/maps/demo_map.txt')
-
+true_scroll = [50, 100]  # x, y
 
 player_image = pygame.image.load('res/sprites/hero/run_1.png').convert()
 player_image.set_colorkey((255, 255, 255))
 
-player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
-test_rect = pygame.Rect(100,100,100,50)
-
-# each item: [k, [x, y, width, height]]
-background_objects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,40,40,400]],[0.5,[130,90,100,400]],[0.5,[300,80,120,400]]]
+player_rect = pygame.Rect(50, 300, player_image.get_width(), player_image.get_height())
+test_rect = pygame.Rect(100, 100, 100, 50)
 
 grass_image = pygame.image.load('res/blocks/grass.png')
 dirt_image = pygame.image.load('res/blocks/dirt.png')
 
-TILE_SIZE = grass_image.get_width()
-    
+bg = pygame.image.load("res/backgrounds/bg_mnt-valley.jpg")
+
+TILE_SIZE = 16
+
 
 def collision_test(rect, tiles):
     hit_list = []
@@ -50,6 +37,7 @@ def collision_test(rect, tiles):
         if rect.colliderect(tile):
             hit_list.append(tile)
     return hit_list
+
 
 def move(rect, movement, tiles):
     collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
@@ -73,52 +61,72 @@ def move(rect, movement, tiles):
             collision_types['top'] = True
     return rect, collision_types
 
+
+def get_tile_properties(tmxdata, x, y):
+    world_x = x
+    world_y = y
+    tile_x = world_x // TILE_SIZE
+    tile_y = world_y // TILE_SIZE
+    try:
+        properties = tmxdata.get_tile_properties(tile_x, tile_y, 0)
+    except ValueError:
+        properties = {"ground": 0}
+    if properties is None:
+        properties = {"ground": 0}
+    return properties
+
+
+tmxdata = load_pygame("res/maps/map.tmx")
+world_offset = [0, 0]
+
 moving_right = False
 moving_left = False
 
 player_y_momentum = 0
 air_timer = 0
 
+bg_x = 0
+bg_y = 0
 
-while True: # game loop
-    display.fill((146,244,255)) # clear screen by filling it with blue
+while True:  # game loop
 
-    true_scroll[0] += (player_rect.x-true_scroll[0]-152)/20
-    true_scroll[1] += (player_rect.y-true_scroll[1]-106)/20
+    true_scroll[0] += (player_rect.x - true_scroll[0] - WINDOW_SIZE[0] // 4) / 14
+    true_scroll[1] += (player_rect.y - true_scroll[1] - WINDOW_SIZE[1] // 4) / 14
     scroll = true_scroll.copy()
     # for images to render correctly
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
 
-    # big most backward rectangle
-    pygame.draw.rect(display,(7,80,75),pygame.Rect(0,120,300,80))
-    for background_object in background_objects:
-        obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0],background_object[1][1]-scroll[1]*background_object[0],background_object[1][2],background_object[1][3])
-        # color based on parallax coefficient
-        if background_object[0] == 0.5:
-            pygame.draw.rect(display,(14,222,150),obj_rect) # brighter green
-        else: # 0.25
-            pygame.draw.rect(display,(9,91,85),obj_rect) # darker green
+    display.blit(bg, (bg_x, bg_y))
+
+    # # big most backward rectangle
+    # pygame.draw.rect(display, (7, 80, 75), pygame.Rect(0, 120, 300, 80))
+    # for background_object in background_objects:
+    #     obj_rect = pygame.Rect(background_object[1][0] - scroll[0] * background_object[0],
+    #                            background_object[1][1] - scroll[1] * background_object[0], background_object[1][2],
+    #                            background_object[1][3])
+    #     # color based on parallax coefficient
+    #     if background_object[0] == 0.5:
+    #         pygame.draw.rect(display, (14, 222, 150), obj_rect)  # brighter green
+    #     else:  # 0.25
+    #         pygame.draw.rect(display, (9, 91, 85), obj_rect)  # darker green
 
     tile_rects = []
-    y = 0
-    for row in game_map:
-        x = 0
-        for tile in row:
-            if tile == '1':
-                display.blit(dirt_image, (x * TILE_SIZE-scroll[0], y * TILE_SIZE-scroll[1]))
-            if tile == '2':
-                display.blit(grass_image, (x * TILE_SIZE-scroll[0], y * TILE_SIZE-scroll[1]))
-            if tile != '0':
-                tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
-            x += 1
-        y += 1
+    for layer in tmxdata:
+        for tile in layer.tiles():
+            x = tile[0] * TILE_SIZE - scroll[0]
+            y = tile[1] * TILE_SIZE - scroll[1]
+            display.blit(tile[2], (x, y))
+            if get_tile_properties(tmxdata, tile[0] * TILE_SIZE, tile[1] * TILE_SIZE)['ground']:
+                tile_rects.append(pygame.Rect(tile[0] * TILE_SIZE, tile[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
     player_movement = [0, 0]
     if moving_right:
         player_movement[0] += 2
+        bg_x -= 2
     if moving_left:
         player_movement[0] -= 2
+        bg_x += 2
     player_movement[1] += player_y_momentum
     player_y_momentum += 0.2
     if player_y_momentum > 3:
@@ -132,12 +140,12 @@ while True: # game loop
     else:
         air_timer += 1
 
-    display.blit(player_image, (player_rect.x-scroll[0], player_rect.y-scroll[1]))
+    display.blit(player_image, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
-    for event in pygame.event.get(): # event loop
-        if event.type == QUIT: # check for window quit
-            pygame.quit() # stop pygame
-            sys.exit() # stop script
+    for event in pygame.event.get():  # event loop
+        if event.type == QUIT:  # check for window quit
+            pygame.quit()  # stop pygame
+            sys.exit()  # stop script
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 moving_right = True
@@ -151,8 +159,7 @@ while True: # game loop
                 moving_right = False
             if event.key == K_LEFT:
                 moving_left = False
-
     surf = pygame.transform.scale(display, WINDOW_SIZE)
     screen.blit(surf, (0, 0))
-    pygame.display.update() # update display
-    clock.tick(60) # maintain 60 fps
+    pygame.display.update()  # update display
+    clock.tick(60)  # maintain 60 fps
---
+++ res/maps/forest.tsx
@@ -0,0 +1,229 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="sheet" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="sheet.png" trans="ffffff" width="272" height="128"/>
+ <tile id="7">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="111">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="ground" value="1"/>
+  </properties>
+ </tile>
+</tileset>
