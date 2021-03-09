3e987e3a2abdb8d85496e209706a62dfcc87c6e9
Sophie P <sonya.pav25@gmail.com>
2021-03-09 15:17:29 +0300

    Fix: minor fixes & code prettifying

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ display = pygame.Surface((WIDTH // 2, HEIGHT // 2))
 
 bg = pygame.image.load("res/backgrounds/bg_mnt-valley.jpg")
 
-map = Map("map.tmx")
+level_map = Map("map.tmx")
 all_sprites = pygame.sprite.Group()
 player = Player(all_sprites, 15, 10)  # x, y: start coord-s
 
@@ -27,7 +27,7 @@ bg_x = 0
 bg_y = 0
 
 while True:  # game loop
-    player.update_scroll(map.width, map.height)
+    player.update_scroll(level_map.width, level_map.height)
     display.blit(bg, (bg_x, bg_y))  # background move (actually no)
 
     # # big most backward rectangle
@@ -42,12 +42,12 @@ while True:  # game loop
     #     else:  # 0.25
     #         pygame.draw.rect(display, (9, 91, 85), obj_rect)  # darker green
 
-    player.tile_rects = map.blit_all_tiles(display, player.scroll)
+    player.tile_rects = level_map.blit_all_tiles(display, player.scroll)
     all_sprites.update()
 
-
     for sprite in all_sprites:
-        display.blit(pg.transform.flip(sprite.image, player.flip, False), (player.rect.x - player.scroll[0], player.rect.y - player.scroll[1]))
+        display.blit(pg.transform.flip(sprite.image, player.flip, False),
+                     (player.rect.x - player.scroll[0], player.rect.y - player.scroll[1]))
 
     for event in pygame.event.get():  # event loop
         if event.type == QUIT:  # check for window quit
