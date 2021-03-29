2eed9b733025623d20cce47b3af175d8597baaaf
Nikita <huskyray@mail.ru>
2021-03-30 01:24:21 +0300

    add: ladder

--- tilemap.py
+++ tilemap.py
@@ -3,6 +3,7 @@ from settings import *
 import pygame
 from pytmx.util_pygame import load_pygame
 from os import path
+from sprites import *
 
 
 class Map:
@@ -14,7 +15,8 @@ class Map:
 
         height: map height in block * TILESIZE
     """
-    def __init__(self, filename):
+
+    def __init__(self, filename, group):
         """
         Constructor for Map
 
@@ -24,6 +26,10 @@ class Map:
         self.tmx_data = load_pygame(path.join("res/maps/", filename))
         self.width = self.tmx_data.width * TILESIZE
         self.height = self.tmx_data.height * TILESIZE
+        self.group = group  # Sprite group
+
+        self.ladders = []
+        self.ladders_loaded = False
 
     def get_tile_properties(self, x, y):
         """
@@ -41,9 +47,13 @@ class Map:
         try:
             properties = self.tmx_data.get_tile_properties(tile_x, tile_y, 0)
         except ValueError:
-            properties = {"ground": 0}
+            properties = {"ground": 0, "ladder": 0}
         if properties is None:
-            properties = {"ground": 0}
+            properties = {"ground": 0, "ladder": 0}
+        keys = ['ground', 'ladder']
+        for key in keys:
+            if properties.get(key) is None:
+                properties.update({key: 0})
         return properties
 
     def blit_all_tiles(self, display, scroll):
@@ -62,6 +72,10 @@ class Map:
                 x = tile[0] * TILESIZE - scroll[0]
                 y = tile[1] * TILESIZE - scroll[1]
                 display.blit(tile[2], (x, y))
-                if self.get_tile_properties(tile[0] * TILESIZE, tile[1] * TILESIZE)['ground']:
+                properties = self.get_tile_properties(tile[0] * TILESIZE, tile[1] * TILESIZE)
+                if properties['ground']:
                     tile_rects.append(pygame.Rect(tile[0] * TILESIZE, tile[1] * TILESIZE, TILESIZE, TILESIZE))
+                if not self.ladders_loaded and properties['ladder']:
+                    self.ladders.append(Ladder(self.group, tile[2], tile[0], tile[1]))
+        self.ladders_loaded = True
         return tile_rects
