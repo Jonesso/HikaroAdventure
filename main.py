8def9c9394baf0aa3e2c5fff7711f4fa5a855308
Nikita <huskyray@mail.ru>
2021-03-30 01:23:02 +0300

    refactoring

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def draw_text(text, font, color, surface, x, y):
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
+
 class Game:
     """
     Main game class, contains all the logic of the game interface
@@ -24,8 +25,8 @@ class Game:
         self.display = pg.Surface((WIDTH // 2, HEIGHT // 2))
         pg.display.set_caption(TITLE)  # set the window name
         self.clock = pg.time.Clock()  # set up the clock
-        #  self.load_data("map")  # TODO add changing levels
         self.new()
+        self.playing = False
 
     def load_data(self, level_name):
         """
@@ -37,8 +38,9 @@ class Game:
         self.all_sprites = pg.sprite.Group()
         self.bg = pg.image.load("res/backgrounds/bg_mnt-valley.jpg")
         self.bg_x = self.bg_y = 0
-        self.level_map = Map("{}.tmx".format(level_name))
-        self.player = Player(self.all_sprites, 15, 10)  # x, y: start coord-s
+        self.level_map = Map("{}.tmx".format(level_name), self.all_sprites)
+        self.player = Player(self.all_sprites, 2, 36)  # x, y: start coord-s
+        self.player.set_map(self.level_map)
         # TODO create a dict for levels and starting coords
 
         # Sounds
@@ -50,16 +52,16 @@ class Game:
         """
         Setup for a new game, initialize variables
         """
+        # Sounds
         pg.mixer.pre_init(44100, -16, 2, 512)  # frequency, size, amount of channels, buffer
         pg.mixer.set_num_channels(64)  # default is 8, which is not enough
 
-        self.jump_sound = pygame.mixer.Sound('res/music/sounds/jump.wav')
-        self.jump_sound.set_volume(0.4)
         grass_sound = [pygame.mixer.Sound('res/music/sounds/grass_0.wav'),
                        pygame.mixer.Sound('res/music/sounds/grass_1.wav')]
         grass_sound[0].set_volume(0.2)
         grass_sound[1].set_volume(0.2)
 
+        # Fonts
         self.font = pygame.font.Font(None, 40)
 
     def quit(self):
@@ -83,8 +85,9 @@ class Game:
         Draws sprites to the display, update pygame display
         """
         for sprite in self.all_sprites:
-            self.display.blit(pg.transform.flip(sprite.image, self.player.flip, False),
-                              (self.player.rect.x - self.player.scroll[0], self.player.rect.y - self.player.scroll[1]))
+            if isinstance(sprite, Player):
+                self.display.blit(pg.transform.flip(sprite.image, self.player.flip, False), (
+                    self.player.rect.x - self.player.scroll[0], self.player.rect.y - self.player.scroll[1]))
         surf = pg.transform.scale(self.display, WINDOW_SIZE)
         self.screen.blit(surf, (0, 0))
         pg.display.update()  # update display
@@ -102,21 +105,10 @@ class Game:
                     pg.mixer.music.fadeout(1000)
                 if event.key == K_e:
                     pg.mixer.music.play(-1)
-                if event.key == K_RIGHT:
-                    self.player.moving_right = True
-                if event.key == K_LEFT:
-                    self.player.moving_left = True
-                if event.key == K_UP:
-                    if self.player.air_timer < 6:
-                        self.jump_sound.play()
-                        self.player.y_momentum = -5
                 if event.key == K_ESCAPE:
                     self.playing = False
-            if event.type == KEYUP:
-                if event.key == K_RIGHT:
-                    self.player.moving_right = False
-                if event.key == K_LEFT:
-                    self.player.moving_left = False
+            if self.playing:
+                self.player.update_event(event)
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.click = True
