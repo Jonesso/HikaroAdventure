0ca76685d61c7602dd9fc83ef2920f0c9ceb8ff5
Nikita <huskyray@mail.ru>
2021-03-30 01:23:18 +0300

    add: sprites.py with all sprites

---
+++ sprites.py
@@ -0,0 +1,27 @@
+import pygame as pg
+from settings import *
+
+
+class Block(pg.sprite.Sprite):
+    def __init__(self, group, sprite, x, y):
+        self.group = group
+        pg.sprite.Sprite.__init__(self, self.group)
+
+        self.image = sprite
+        self.image.set_colorkey(WHITE)
+
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+
+class Ladder(Block):
+    def __init__(self, groups, sprite, x, y):
+        super(Ladder, self).__init__(groups, sprite, x, y)
+
+
+class Ground(Block):
+    def __init__(self, groups, sprite, x, y):
+        super(Ground, self).__init__(groups, sprite, x, y)
